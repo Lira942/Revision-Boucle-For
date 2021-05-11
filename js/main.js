038f8eb6517af444c8b18d5612ffee2dfcbf3b86
Lira942 <liracanda@gmail.com>
2021-05-11 19:32:36 +0200

    Revision Boucle For

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
-let prenom;
-let classe =[];
+// let prenom;
+// let classe =[];
 
-for( let i = 0; i<10; i++){
-    prenom= prompt("Donne moi un nom!")
-    classe.push(prenom);
-    console.log(`Bonjour ${classe[i]}`);
+// for( let i = 0; i<10; i++){
+//     prenom= prompt("Donne moi un nom!")
+//     classe.push(prenom);
+//     console.log(`Bonjour ${classe[i]}`);
+// }
 
-}
 // EXO2
+// let tab = ["Adil", "Agim", "Mouna", "Nathan", "Seif", "Nasila", "Elvis", "Ali", "Elias 1 ", "Elias 2"];
+// for(let i = 0; i < tab.length; i++){
+//     if(tab[i].charAt(0) =="A"){
+//        console.log(`Yo ${tab[i]}`);
+//     }else if(tab[i].charAt(0) =="E"){
+//         console.log(`Salut ${tab[i]}`);
+//     }else{
+//         console.log(`Ca gaze ${tab[i]}`);
+//     }
+// }
+
+// Exo3
+ let array =["chateau", "moto", "velo", "ballon"];
+ for(let i = 0; i < array.length; i++){
+    if(array[i]){
+
+    }
+ }
+   
+
+// EXO4
+// let mdp = prompt("Entrez votre mot de passe !");
+// for(let i = mdp; i = "Lira"; alert("Acces autorisé"){}
